52fce3f2757a5dedf88a40cd191880391ec686d6
mkuhtze-code <mkuhtze@gmail.com>
2026-07-20 09:34:50 +1200

    Initialize webpush with VAPID details

--- lib/webpush.ts
+++ lib/webpush.ts
@@ -1,1 +1,9 @@
+import webpush from 'web-push';
 
+webpush.setVapidDetails(
+  process.env.VAPID_SUBJECT as string,
+  process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY as string,
+  process.env.VAPID_PRIVATE_KEY as string
+);
+
+export default webpush;
