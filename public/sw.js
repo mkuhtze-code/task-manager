75ae992c03daf55bc534a3b78e1e7f1c1fc30a86
mkuhtze-code <mkuhtze@gmail.com>
2026-07-20 09:33:11 +1200

    Add push notification handling in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,32 @@
+self.addEventListener('push', function(event) {
+  var data = { title: 'Task Manager', body: 'You have a notification.' };
+  if (event.data) {
+    try {
+      data = event.data.json();
+    } catch (e) {
+      data.body = event.data.text();
+    }
+  }
+  var options = {
+    body: data.body,
+    icon: '/icon-192.png',
+    badge: '/icon-192.png',
+  };
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
 
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        if (clientList[i].url.indexOf(self.location.origin) === 0 && 'focus' in clientList[i]) {
+          return clientList[i].focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
+  );
+});
