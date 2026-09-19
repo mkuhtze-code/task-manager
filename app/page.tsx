f465fdbdb4266400280778003068194b7fb8f8df
mkuhtze-code <mkuhtze@gmail.com>
2026-09-19 21:14:45 +1200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -94,6 +94,7 @@ export default function Home() {
     forgotPasswordSent,
     setForgotPasswordSent,
     signInError,
+    setSignInError, 
     hasSignedInBefore,
     isNewUser,
     setIsNewUser,
