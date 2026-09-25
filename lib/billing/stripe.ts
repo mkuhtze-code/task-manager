a297f77f355a16e88caff0bd5ac0499cc5129d4d
mkuhtze-code <mkuhtze@gmail.com>
2026-09-25 20:16:52 +1200

    Update Stripe API version to 2025-02-24

--- lib/billing/stripe.ts
+++ lib/billing/stripe.ts
@@ -9,7 +9,7 @@ export function getStripe(): Stripe {
   }
   if (!stripe) {
     stripe = new Stripe(key, {
-      apiVersion: '2024-11-20.acacia',
+      apiVersion: '2025-02-24.acacia',
       typescript: true,
     });
   }
