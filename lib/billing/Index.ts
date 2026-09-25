d6ba25eae66ad9b1a5aa2a7058d405beb8b9e6e7
mkuhtze-code <mkuhtze@gmail.com>
2026-09-25 20:07:30 +1200

    Delete lib/billing/Index.ts

--- lib/billing/Index.ts
+++
@@ -1,4 +0,0 @@
-export type { AccountTier, BillingStatus, Entitlements } from './types';
-export { PLAN_COPY } from './types';
-export { resolveEntitlements, tierFromStripeStatus } from './entitlements';
-export { getStripe, getStripePriceMonthly, appBaseUrl, isStripeConfigured } from './stripe';
