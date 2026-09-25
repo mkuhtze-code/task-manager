b0e009896229a1554508ca02e2ec08f8665c0051
mkuhtze-code <mkuhtze@gmail.com>
2026-09-25 20:08:11 +1200

    Export types and functions from billing module

--- lib/billing/index.ts
+++ lib/billing/index.ts
@@ -1,1 +1,4 @@
-
+export type { AccountTier, BillingStatus, Entitlements } from './types';
+export { PLAN_COPY } from './types';
+export { resolveEntitlements, tierFromStripeStatus } from './entitlements';
+export { getStripe, getStripePriceMonthly, appBaseUrl, isStripeConfigured } from './stripe';
