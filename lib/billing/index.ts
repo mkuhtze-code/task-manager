f547c7efe2b425a1fd86665b0317ea4b1b2a2113
mkuhtze-code <mkuhtze@gmail.com>
2026-09-26 12:36:30 +1200

    Enhance exports in billing module with new features

--- lib/billing/index.ts
+++ lib/billing/index.ts
@@ -1,8 +1,16 @@
-export type { AccountTier, BillingStatus, Entitlements } from './types';
-export { PLAN_COPY } from './types';
-export { resolveEntitlements, tierFromStripeStatus } from './entitlements';
+export type { AccountTier, BillingStatus, Entitlements, ProFeature } from './types';
+export { PLAN_COPY, PRO_FEATURE_COPY } from './types';
+export {
+  resolveEntitlements,
+  tierFromStripeStatus,
+  canUseFeature,
+} from './entitlements';
 export { getStripe, getStripePriceMonthly, appBaseUrl, isStripeConfigured } from './stripe';
 
 export type { BillingSummary, BillingInvoiceRow } from './summaryTypes';
 
-export { loadEntitlementsForUser, assertCanUseMeetings } from './serverEntitlements';
+export {
+  loadEntitlementsForUser,
+  assertCanUseMeetings,
+  assertCanUseFeature,
+} from './serverEntitlements';
